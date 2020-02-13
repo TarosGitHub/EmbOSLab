f111614d30ba68028bae515efa005a8d725d2749
Taro <no_taro1990@outlook.com>
2020-02-13 15:19:17 +0000

    Try writting the global variable

--- src/bootload/main.c
+++ src/bootload/main.c
@@ -2,15 +2,20 @@
 #include "serial.h"
 #include "lib.h"
 
+volatile int value = 10;
+
 int main(void)
 {
     serial_init(SERIAL_DEFAULT_DEVICE);
 
     puts("Hello World!\n");
 
-    putxval(0x10, 0);
+    putxval(value, 0);
     puts("\n");
-    putxval(0xffff, 0);
+
+    value = 20;
+
+    putxval(value, 0);
     puts("\n");
 
     while (1) {
