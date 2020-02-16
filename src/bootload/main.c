4286d49289b4228864fdd8f05e3e69678927ba5b
Taro <no_taro1990@outlook.com>
2020-02-16 11:36:15 +0000

    Define static variables in data and bss section

--- src/bootload/main.c
+++ src/bootload/main.c
@@ -2,8 +2,18 @@
 #include "serial.h"
 #include "lib.h"
 
+int a1 = 1;
+static int a2 = 2;
+int a3;
+static int a4;
+
 int main(void)
 {
+    int a5;
+    int a6 = 6;
+    static int a7 = 7;
+    static int a8;
+
     serial_init(SERIAL_DEFAULT_DEVICE);
 
     puts("Hello World!\n");
