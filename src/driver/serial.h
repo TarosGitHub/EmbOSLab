dc5c33965fe4264893e78d2abe6c6e9f1bae67d8
Taro Hoshino <no.taro1990@gmail.com>
2020-10-31 19:26:44 +0900

    7th: os interrupt

--- src/driver/serial.h
+++ src/driver/serial.h
@@ -12,7 +12,7 @@
 extern int serial_init(int index);
 
 /**
- * @brief 送信可能かどうか判断する.
+ * @brief 送信可能かどうか判定する.
  * 
  * @param index SCIチャネル番号
  * @return 送信可能の場合は1. そうでない場合は0を返す.
@@ -29,7 +29,7 @@ extern int serial_is_send_enable(int index);
 extern int serial_send_byte(int index, unsigned char c);
 
 /**
- * @brief 受信可能かどうか判断する.
+ * @brief 受信可能かどうか判定する.
  * 
  * @param index SCIチャネル番号
  * @return 受信可能の場合は1. そうでない場合は0を返す.
@@ -43,5 +43,49 @@ extern int serial_is_recv_enable(int index);
  * @return 受信した文字.
  */
 extern unsigned char serial_recv_byte(int index);
+
+/**
+ * @brief 送信割込みが有効かどうか判定する.
+ * 
+ * @param index SCIチャネル番号
+ * @return 割込みが有効の場合は1. そうでない場合は0を返す.
+ */
+extern int serial_intr_is_send_enable(int index);
+
+/**
+ * @brief 送信割込み有効化.
+ * 
+ * @param index SCIチャネル番号
+ */
+extern void serial_intr_send_enable(int index);
+
+/**
+ * @brief 送信割込み無効化.
+ * 
+ * @param index SCIチャネル番号
+ */
+extern void serial_intr_send_disable(int index);
+
+/**
+ * @brief 受信割込みが有効かどうか判定する.
+ * 
+ * @param index SCIチャネル番号
+ * @return 割込みが有効の場合は1. そうでない場合は0を返す.
+ */
+extern int serial_intr_is_recv_enable(int index);
+
+/**
+ * @brief 受信割込み有効化.
+ * 
+ * @param index SCIチャネル番号
+ */
+extern void serial_intr_recv_enable(int index);
+
+/**
+ * @brief 受信割込み無効化.
+ * 
+ * @param index SCIチャネル番号
+ */
+extern void serial_intr_recv_disable(int index);
 
 #endif
--- src/os/main.c
+++ src/os/main.c
@@ -1,30 +1,52 @@
 #include "defines.h"
+#include "intr.h"
+#include "interrupt.h"
 #include "serial.h"
 #include "lib.h"
 
-int main(void)
+static void intr(softvec_type_t type, unsigned long sp)
 {
+    int c;
     static char command[32];
+    static int len;
 
-    puts("Hello World\n");
+    c = getchar();
 
-    while (1) {
-        /* コマンド受付 */
-        puts("> ");
-        gets(command);
+    if (c != '\n') {
+        command[len++] = c;
+    }
+    else {
+        command[len++] = '\0';
 
         if (!strncmp(command, "echo", 4)) {
             /* echo コマンド実行 */
             puts(command + 4);
             puts("\n");
         }
-        else if (!strcmp(command, "exit")) {
-            /* exit コマンド実行 */
-            break;
-        }
         else {
-            puts("unknown.\n");
+            puts("unknown\n");
         }
+
+        puts("> ");
+        len = 0;
+    }
+}
+
+int main(void)
+{
+    INTR_DISABLE();
+
+    puts("kozos boot succeed!\n");
+
+    softvec_setintr(SOFTVEC_TYPE_SERINTR, intr);
+    serial_intr_recv_enable(SERIAL_DEFAULT_DEVICE);
+
+    puts("> ");
+
+    INTR_ENABLE();
+
+    while (1) {
+        asm volatile ("sleep");
     }
 
     return 0;
